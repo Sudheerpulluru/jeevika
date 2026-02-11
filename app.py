644496b1d2ffb22540f59513519b65046e2d98d0
Sudheerpulluru <sudheerpulluru4@gmail.com>
2026-02-12 00:08:00 +0530

    Connected PostgreSQL for production

--- app.py
+++ app.py
@@ -25,7 +25,17 @@ if DATABASE_URL:
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 else:
     # Local development fallback
+    database_url = os.environ.get("DATABASE_URL")
+
+if database_url:
+    # Render gives postgres:// but SQLAlchemy needs postgresql://
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+else:
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///jeevika.db"
+
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
